474aec4b20238cc132df7aea799097efff701e6a
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-06-07 03:55:34 -0400

    Added hover highlighting

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let previewLevel = 0;
+let previewLevel = false;
 let globalEditZoom = 1.0;
 
 let zoomConstraints = {
@@ -11,44 +11,73 @@ let translationAmount;
 let editOffsetX = 0;
 let editOffsetY = 0;
 
+let currentHover;
+
+let currentLevel = {
+    
+    palletes:[]
+}
+
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    translationAmount = createVector(40, 50);
+    translationAmount = createVector(0, 0);
+    currentHover = createVector(0, 0);
 }
 
 function draw() {
     createCanvas(innerWidth, innerHeight);
-    frameRate(30);
+    frameRate(60);
     background(255);
-    
     if (previewLevel) {
         levelDraw()
     } else {
-        //cookiesEncode("test123456, my face hurts");
+        //cookiesEncode("test123");
         //console.log(cookiesGet());
         
-        translationAmount.x = width / 2;
-        translationAmount.y = height / 2
-        translate(translationAmount.x, translationAmount.y);
-        scaleAmount = height / (400 * globalEditZoom);
-        scale(scaleAmount);
-        translate(editOffsetX, editOffsetY);
-        for(let yInd = 0; yInd < 20; yInd ++) {
-            for(let xInd = 0; xInd < 20; xInd ++ ) {
-                rect((xInd - 10) * 20, (yInd - 10) * 20, 20, 20)
-            }
-        }
+        editDraw();
         
-        translate(-editOffsetX, -editOffsetY);
-        scale(1/scaleAmount);
-        translate(-translationAmount.x, -translationAmount.y);
     }
     
+    fill(0, 0, 0);
     text("Zoom: " + (1/globalEditZoom * 100 + "").substr(0, 5) + "%", width - 80, height - 5);
 }
 
 function editDraw(levelObject, type, pallete, objectsObject){
+    translationAmount.x = width / 2;
+    translationAmount.y = height / 2
+    scaleAmount = height / (400 * globalEditZoom);
     
+    currentHover.x = (Math.floor(((mouseX - translationAmount.x) / scaleAmount - editOffsetX)/20) <= 9 && Math.floor(((mouseX-translationAmount.x)/scaleAmount-editOffsetX)/20) >= -10) ? Math.floor(((mouseX-translationAmount.x)/scaleAmount-editOffsetX)/20)+10 : undefined;
+    currentHover.y = (Math.floor(((mouseY - translationAmount.y) / scaleAmount - editOffsetY)/20) <= 9 && Math.floor(((mouseY-translationAmount.y)/scaleAmount-editOffsetY)/20) >= -10) ? Math.floor(((mouseY-translationAmount.y)/scaleAmount-editOffsetY)/20)+10 : undefined;
+    
+    translate(translationAmount.x, translationAmount.y);
+    scale(scaleAmount);
+    translate(editOffsetX, editOffsetY);
+    
+    
+    drawField2D(levelObject, type, pallete, objectsObject);
+    
+    
+    translate(-editOffsetX, -editOffsetY);
+    scale(1/scaleAmount);
+    translate(-translationAmount.x, -translationAmount.y);
+    
+    
+    
+    fill(0, 0, 0);
+    
+}
+
+function drawField2D(levelObject, type, pallete, objectsObject){
+    for(let yInd = 0; yInd < 20; yInd ++) {
+        for(let xInd = 0; xInd < 20; xInd ++ ) {
+            fill(yInd * 10, xInd * 10, yInd * 10);
+            if(xInd === currentHover.x && yInd === currentHover.y){
+                fill(yInd * 10 * 0.6 + 255 * 0.4, xInd * 10 * 0.6 + 255 * 0.4, yInd * 10 * 0.6 + 255 * 0.4);
+            }
+            rect((xInd - 10) * 20, (yInd - 10) * 20, 20, 20);
+        }
+    }
 }
 
 function levelDrawWebGL(levelObject, type, pallete, objectsObject){
@@ -66,8 +95,6 @@ function mouseWheel(event) {
     globalEditZoom = constrain(globalEditZoom, zoomConstraints.edit[0], zoomConstraints.edit[1]);
     return false;
 }
-
-
 
 function mouseDragged(event) {
     scaleAmount = height / (400 * globalEditZoom);
