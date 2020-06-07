1e8007f93fc46e8e9684440f67d9ff1bc03dc887
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-06-07 03:09:53 -0400

    Added panning

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-function setup() {
-    createCanvas(innerWidth, innerHeight);
-}
-
 let previewLevel = 0;
-let no = 0;
 let globalEditZoom = 1.0;
 
 let zoomConstraints = {
     edit:[0.1, 2]
+}
+
+let scaleAmount = 0;
+let translationAmount;
+
+let editOffsetX = 0;
+let editOffsetY = 0;
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    translationAmount = createVector(40, 50);
 }
 
 function draw() {
@@ -21,22 +27,24 @@ function draw() {
         //cookiesEncode("test123456, my face hurts");
         //console.log(cookiesGet());
         
-        console.log(1 / globalEditZoom);
-        translate(width / 2, height / 2);
-        
-        scale(1/globalEditZoom * height / 400);
-        
+        translationAmount.x = width / 2;
+        translationAmount.y = height / 2
+        translate(translationAmount.x, translationAmount.y);
+        scaleAmount = height / (400 * globalEditZoom);
+        scale(scaleAmount);
+        translate(editOffsetX, editOffsetY);
         for(let yInd = 0; yInd < 20; yInd ++) {
             for(let xInd = 0; xInd < 20; xInd ++ ) {
                 rect((xInd - 10) * 20, (yInd - 10) * 20, 20, 20)
             }
         }
         
-        scale(globalEditZoom * 400 / height);
+        translate(-editOffsetX, -editOffsetY);
+        scale(1/scaleAmount);
+        translate(-translationAmount.x, -translationAmount.y);
     }
     
-    translate(width / 2, height / 2)
-    text("Zoom: " + (1/globalEditZoom * 100 + "").substr(0, 5) + "%", -80, -5)
+    text("Zoom: " + (1/globalEditZoom * 100 + "").substr(0, 5) + "%", width - 80, height - 5);
 }
 
 function editDraw(levelObject, type, pallete, objectsObject){
@@ -54,14 +62,21 @@ function levelDrawWebGL(levelObject, type, pallete, objectsObject){
 
 
 function mouseWheel(event) {
-    globalEditZoom += globalEditZoom * 0.0004 * event.delta; 
+    globalEditZoom += globalEditZoom * 0.0005 * event.delta; 
     globalEditZoom = constrain(globalEditZoom, zoomConstraints.edit[0], zoomConstraints.edit[1]);
     return false;
 }
 
 
 
-
+function mouseDragged(event) {
+    scaleAmount = height / (400 * globalEditZoom);
+    editOffsetX += event.movementX / scaleAmount;
+    editOffsetY += event.movementY / scaleAmount;
+    editOffsetX = constrain(editOffsetX, -190, 190)
+    editOffsetY = constrain(editOffsetY, -190, 190)
+    return(false);
+}
 
 
 
